d74b883d04443dc8c3b1f3a394ec6f49168f18f3
Igor Permyakov <imouttasightx@gmail.com>
2020-10-03 02:29:07 +0600

    Added login/logout pages and views, dashboard, 'is_logged_in' decorator

--- app/app.py
+++ app/app.py
@@ -9,6 +9,7 @@ from flask import request
 from flask_mysqldb import MySQL
 from wtforms import Form, StringField, TextAreaField, PasswordField, validators
 from passlib.hash import sha256_crypt
+from functools import wraps
 
 from data import Articles
 
@@ -24,7 +25,7 @@ app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
 # Init MySQL
 mysql = MySQL(app)
 
-
+# Temporary articles
 Articles = Articles()
 
 
@@ -32,17 +33,21 @@ Articles = Articles()
 def index():
     return render_template('home.html')
 
+
 @app.route('/about')
 def about():
     return render_template('about.html')
+
 
 @app.route('/articles')
 def articles():
     return render_template('articles.html', articles=Articles)
 
+
 @app.route('/article/<string:id>/')
 def article(id):
     return render_template('article.html', id=id)
+
 
 class RegisterForm(Form):
     name = StringField('Name', validators=[validators.Length(min=1, max=50)])
@@ -53,6 +58,7 @@ class RegisterForm(Form):
         validators.EqualTo('confirm', message='Passwords do not match')
     ])
     confirm = PasswordField('Confirm password')
+
 
 @app.route('/register', methods=['POST', 'GET'])
 def register():
@@ -66,32 +72,93 @@ def register():
         # Create cursor
         cur = mysql.connection.cursor()
 
+        # Validate email and pass
         cur.execute('SELECT * FROM users')
         users = cur.fetchall()
-
         for u in users:
             if email == u['email']:
                 flash('This email already exists', 'danger')
-                return redirect(url_for('register'))
+                return render_template('register.html', form=form)
             elif username == u['username']:
                 flash('This username already exists', 'danger')
-                return redirect(url_for('register'))
-                
+                return render_template('register.html', form=form)      
+        
         # Execute query
         cur.execute("INSERT INTO users(name, email, username, password)\
                     VALUES(%s, %s, %s, %s)", (name, email, username, password))
 
         # Commit db
         mysql.connection.commit()
-
         # Close connection
         cur.close()
 
         flash('You are now registered and can log in', 'success')
-
         return redirect(url_for('index'))
     
     return render_template('register.html', form=form)
+
+
+@app.route('/login', methods=['POST', 'GET'])
+def login():
+    if request.method == 'POST':
+        # Get form fieds
+        username = request.form['username']
+        password_candidate = request.form['password']
+
+        # Create cursor
+        cur = mysql.connection.cursor()
+
+        # Get user by username
+        result = cur.execute("SELECT * FROM users WHERE username = %s", [username])
+
+        if result > 0:
+            # Get stored hash
+            data = cur.fetchone()
+            password = data['password']
+
+            # Compare passwords
+            if sha256_crypt.verify(password_candidate, password):
+                app.logger.info('PASSWORD MATCHED')
+                session['logged_in'] = True
+                session['username'] = username
+                flash('You are now logged in', 'success')
+                return redirect(url_for('dashboard'))
+            else:
+                #error = 'Invalid login'
+                flash('Invalid login', 'danger')
+                return render_template('login.html')
+
+            cur.close()
+        else:
+            #error = 'Username not found'
+            flash('Username not found', 'danger')
+            return render_template('login.html')
+
+    return render_template('login.html')
+
+
+@app.route('/logout')
+def logout():
+    session.clear()
+    flash('You are now logged out', 'success')
+    return redirect(url_for('login'))
+
+
+def is_logged_in(f):
+    @wraps(f)
+    def wrapper(*args, **kwargs):
+        if 'logged_in' in session:
+            return f(*args, **kwargs)
+        else:
+            flash('Unauthorized. Please, log in', 'danger')
+            return redirect(url_for('login'))
+    return wrapper
+
+
+@app.route('/dashboard')
+@is_logged_in
+def dashboard():
+    return render_template('dashboard.html')
 
 
 if __name__ == "__main__":
