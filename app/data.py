5b285cd7330228dc8b8f7147815909f53a2bc69f
Igor Permyakov <imouttasightx@gmail.com>
2020-10-02 20:42:25 +0600

    Created some views and templates for them

---
+++ app/data.py
@@ -0,0 +1,25 @@
+def Articles():
+    articles = [
+        {
+            'id': 1,
+            'title': 'Article One',
+            'body': 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Natus accusantium ipsam sint vitae nam voluptatum exercitationem, aliquid magni nihil ipsa eveniet dignissimos at repellendus dolore, maxime eum tempore facilis quisquam!',
+            'author': 'A. Person',
+            'create_date': '10-02-2020',
+        },
+        {
+            'id': 2,
+            'title': 'Article Two',
+            'body': 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Natus accusantium ipsam sint vitae nam voluptatum exercitationem, aliquid magni nihil ipsa eveniet dignissimos at repellendus dolore, maxime eum tempore facilis quisquam!',
+            'author': 'Jim Halpert',
+            'create_date': '10-02-2020',
+        },
+        {
+            'id': 3,
+            'title': 'Article Three',
+            'body': 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Natus accusantium ipsam sint vitae nam voluptatum exercitationem, aliquid magni nihil ipsa eveniet dignissimos at repellendus dolore, maxime eum tempore facilis quisquam!',
+            'author': 'A. Person',
+            'create_date': '10-02-2020',
+        }
+    ]
+    return articles
