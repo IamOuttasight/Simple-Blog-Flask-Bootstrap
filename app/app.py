1b323e9ca48cd83f7987eee7de61135a0a3303ef
Igor Permyakov <imouttasightx@gmail.com>
2020-10-03 16:09:27 +0600

    Created 'articles' table, 'ArticleForm', modified templates related to that table, created 'add_article' view

--- app/app.py
+++ app/app.py
@@ -11,7 +11,7 @@ from wtforms import Form, StringField, TextAreaField, PasswordField, validators
 from passlib.hash import sha256_crypt
 from functools import wraps
 
-from data import Articles
+#from data import Articles
 
 
 app = Flask(__name__)
@@ -26,7 +26,7 @@ app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
 mysql = MySQL(app)
 
 # Temporary articles
-Articles = Articles()
+#Articles = Articles()
 
 
 @app.route('/')
@@ -41,12 +41,22 @@ def about():
 
 @app.route('/articles')
 def articles():
-    return render_template('articles.html', articles=Articles)
+    cur = mysql.connection.cursor()
+    result = cur.execute("SELECT * FROM articles")
+    articles = cur.fetchall()
+    cur.close()
+    if result > 0:
+        return render_template('articles.html', articles=articles)
+    else:
+        return render_template('articles.html')
 
 
 @app.route('/article/<string:id>/')
 def article(id):
-    return render_template('article.html', id=id)
+    cur = mysql.connection.cursor()
+    cur.execute("SELECT * FROM articles WHERE id=%s", [id])
+    article = cur.fetchone()
+    return render_template('article.html', article=article)
 
 
 class RegisterForm(Form):
@@ -115,6 +125,7 @@ def login():
             # Get stored hash
             data = cur.fetchone()
             password = data['password']
+            cur.close()
 
             # Compare passwords
             if sha256_crypt.verify(password_candidate, password):
@@ -127,21 +138,13 @@ def login():
                 #error = 'Invalid login'
                 flash('Invalid login', 'danger')
                 return render_template('login.html')
-
-            cur.close()
         else:
+            cur.close()
             #error = 'Username not found'
             flash('Username not found', 'danger')
             return render_template('login.html')
 
     return render_template('login.html')
-
-
-@app.route('/logout')
-def logout():
-    session.clear()
-    flash('You are now logged out', 'success')
-    return redirect(url_for('login'))
 
 
 def is_logged_in(f):
@@ -155,10 +158,48 @@ def is_logged_in(f):
     return wrapper
 
 
+@app.route('/logout')
+@is_logged_in
+def logout():
+    session.clear()
+    flash('You are now logged out', 'success')
+    return redirect(url_for('login'))
+
+
 @app.route('/dashboard')
 @is_logged_in
 def dashboard():
-    return render_template('dashboard.html')
+    cur = mysql.connection.cursor()
+    result = cur.execute("SELECT * FROM articles")
+    articles = cur.fetchall()
+    cur.close()
+    if result > 0:
+        return render_template('dashboard.html', articles=articles)
+    else:
+        return render_template('dashboard.html')
+
+class ArticleForm(Form):
+    title = StringField('Title', validators=[validators.Length(min=1, max=205)])
+    body = TextAreaField('Content', validators=[validators.Length(min=30)])
+
+
+@app.route('/add_article', methods=['POST', 'GET'])
+@is_logged_in
+def add_article():
+    form = ArticleForm(request.form)
+    if request.method == 'POST' and form.validate():
+        title = form.title.data
+        body = form.body.data
+
+        cur = mysql.connection.cursor()
+        cur.execute("INSERT INTO articles(title, body, author) VALUES(%s, %s, %s)", (title, body, session['username']))
+        mysql.connection.commit()
+        cur.close()
+
+        flash('Article created', 'success')
+        return redirect(url_for('dashboard'))
+
+    return render_template('add_article.html', form=form)
 
 
 if __name__ == "__main__":
