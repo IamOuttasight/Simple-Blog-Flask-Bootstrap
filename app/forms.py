b16be1b70a980e185816b347199e2a18f9d2e8fd
Igor Permyakov <imouttasightx@gmail.com>
2020-10-09 17:04:50 +0600

    Improved form validation

--- app/forms.py
+++ app/forms.py
@@ -3,15 +3,23 @@ from wtforms import StringField
 from wtforms import TextAreaField
 from wtforms import PasswordField
 from wtforms import validators
+from wtforms.fields.html5 import EmailField
 
 
 class RegisterForm(Form):
     name = StringField('Name', validators=[validators.Length(min=1, max=50)])
-    username = StringField('Username', validators=[validators.Length(min=4, max=25)])
-    email = StringField('Email', validators=[validators.Length(min=6, max=50)])
+    username = StringField('Username', validators=[
+        validators.Length(min=4, max=25),
+        validators.Regexp(r'^[a-zA-Z0-9_-]+$',
+            message='Only letters a-z and A-Z, digits and symbols -_ are allowed.')
+    ])
+    email = EmailField('Email', validators=[
+        validators.Length(min=6, max=50),
+        validators.Email(message='Invalid email address.')
+    ])
     password = PasswordField('Password', validators=[
         validators.DataRequired(),
-        validators.EqualTo('confirm', message='Passwords do not match')
+        validators.EqualTo('confirm', message='Passwords do not match.')
     ])
     confirm = PasswordField('Confirm password')
 
